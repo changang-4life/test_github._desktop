0cb4f8e14a026afe32be27111f095f687832b50d
changang-4life <akinboj2@middleton.school.nz>
2024-03-14 09:33:19 +1300

    Create moveie_tickets.py

---
+++ moveie_tickets.py
@@ -0,0 +1,14 @@
+# Movie theatre ticketing system - v1
+# Welcome screen and initialise variables
+
+def sell_tickets():
+    print("************ Fanfare Movies - ticketing system ************")
+    adult_tickets = 0
+    student_tickets = 0
+    child_tickets = 0
+    gift_tickets = 0
+    tickets_sold = 0
+    total_sold = 0
+    
+# Main routine
+sell_tickets()
